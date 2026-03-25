80ddef4a352d235198fac6598c869cf19e16d279
a20240076 <a20240076@utem.edu.mx>
2026-03-25 11:27:21 -0600

    cambio de server corriendo

--- backend/server.js
+++ backend/server.js
@@ -111,4 +111,8 @@ app.get('/api/pedidos', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
